756e2e1d3bae32d844d88fb8dff62dfe96ce6cbe
deepwaterooo <heyanh@vandals.uidaho.edu>
2014-12-20 16:16:43 -0800

    minStack

--- minStack.cpp
+++ minStack.cpp
@@ -5,24 +5,43 @@
 #include <cmath>
 using namespace std;
 
-void push(int x) {
+class MinStack {
+private:
+    stack<int> sta;
+    stack<int> minsta;
+public:
+    void push(int x) {
+        sta.push(x);
+        if ( (!minsta.empty() && x <= minsta.top()) || minsta.empty() )
+            minsta.push(x);
+    }
 
-}
+    void pop() {
+        if (!sta.empty())  {
+            if (minsta.top() == sta.top())
+                minsta.pop();
+            sta.pop();
+        }
+    }
 
-void pop() {
+    int top() {
+        return sta.top();
+    }
 
-}
-
-int top() {
-
-}
-
-int getMin() {
-
-}
+    int getMin() {
+        return minsta.top();
+    }
+    
+};
 
 int main(){
-
+    MinStack sta;
+    sta.push(0);
+    sta.push(1);
+    sta.push(0);
+    cout << "getMin: " << sta.getMin() << endl;
+    sta.pop();
+    cout << "getMin: " << sta.getMin() << endl;
 
     return 0;
 }
