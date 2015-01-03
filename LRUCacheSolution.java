36ff7f2579384beeae2759cd31ceb67254fe0fbf
deepwaterooo <heyanh@vandals.uidaho.edu>
2015-01-02 17:51:05 -0800

    headache seek for better me

---
+++ LRUCacheSolution.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LRUCacheSolution {
+    public class LRUCache {
+    
+        public LRUCache(int capacity) {
+        
+        }
+    
+        public int get(int key) {
+        
+        }
+    
+        public void set(int key, int value) {
+        
+        }
+    }
+
+    public static void main(String[] args){
+        Solution result = new LRUCache();
+
+        System.out.println(res);
+    }
+}
