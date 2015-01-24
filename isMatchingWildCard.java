26834a94925980d222f5bddf71f31d33fbcc4cd5
deepwaterooo <heyanh@vandals.uidaho.edu>
2015-01-23 23:33:16 -0800

    workonit...

---
+++ isMatchingWildCard.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+
+public class isMatchingWildCard {
+    public static class Solution {
+        public boolean isMatch(String s, String p) {
+            
+        }
+    }
+
+    public static void main(String[] args){
+        Solution result = new Solution();
+
+        System.out.println(res);
+    }
+}
