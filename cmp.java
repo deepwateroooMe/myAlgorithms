ecc78df35fb9f0f895d1769beeef9ba4cd5031b7
deepwateroooMe <deepwaterooome@gmail.com>
2024-07-07 15:32:03 -0700

    LOVE MY DEAR COUSIN. WAITING FOR MARRIAGE...MUST MARRY HIM AS SOON AS POSSIBLE~~ Make initial build. work on it when having usb port

--- cmp.java
+++ cmp.java
@@ -236,61 +236,317 @@ public class cmp {
 		// 	return ans;
 		// }
 
+		// // 【亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹，就是一定要、一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+		// public int minimumDiameterAfterMerge(int[][] ea, int[][] eb) {
+		// 	n = ea.length+1; m = eb.length+1;
+		// 	f = new ArrayList[n]; g = new ArrayList[m];
+		// 	Arrays.setAll(f, z -> new ArrayList<>());
+		// 	Arrays.setAll(g, z -> new ArrayList<>());
+		// 	if (n > 1) buildTree(ea, f);
+		// 	if (m > 1) buildTree(eb, g);
+		// 	// assume 根节点在 0 节点：求最大、次大深度
+		// 	l = new int [2]; r = new int [2];
+		// 	Arrays.fill(l, -1); Arrays.fill(r, -1);
+		// 	maxDepth(f, l, 0, 0, -1); maxDepth(g, r, 0, 0, -1);
+		// 	System.out.println(Arrays.toString(l));
+		// 	System.out.println(Arrays.toString(r));
+		// 	if (l[1] == -1) l[0] = l[0] / 2 + l[0] % 2;
+		// 	if (r[1] == -1) r[0] = r[0] / 2 + r[0] % 2;
+		// 	return l[0] + 1 + r[0]; // 这里还有点儿机关，没想透彻。。。
+		// }
+		// List<Integer> [] f, g;
+		// int m, n;
+		// int [] l, r;
+		// void maxDepth(List<Integer> [] f, int [] l, int d, int u, int p) {
+		// 	int cnt = 0;
+		// 	for (int v : f[u]) {
+		// 		if (v == p) continue;
+		// 		cnt++;
+		// 		maxDepth(f, l, d+1, v, u);
+		// 	}
+		// 	if (cnt == 0) {
+		// 		if (l[0] == -1) l[0] = d;
+		// 		else if (d >= l[0]) {
+		// 			if (d != l[0])
+		// 				l[1] = l[0];
+		// 			if (d > l[0])
+		// 				l[0] = d;
+		// 		} else if (d > l[1])
+		// 			l[1] = d;
+		// 	}
+		// }
+		// void buildTree(int [][] a, List<Integer> [] f) {
+		// 	for (int [] e : a) {
+		// 		int u = e[0], v = e[1];
+		// 		f[u].add(v);
+		// 		f[v].add(u);
+		// 	}
+		// }
+
+		// // 【亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹，就是一定要、一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+		// public int numberOfAlternatingGroups(int[] a) {
+		// 	int n = a.length, r = 0;
+
+		// 	for (int i = 0; i < n; i++) {
+		// 		if (i < n-1 && a[i] == a[i+1] || i == n-1 && a[i] == a[0]) continue;
+		// 		if (i == 0 && a[i] != a[n-1] || i != 0 && a[i] != a[i-1]) r++;
+		// 	}
+		// 	return r;
+		// }
+
+		// // 【亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹，就是一定要、一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+		// // 想不明白，这个破烂题目算是怎么回事，晚点儿再写这个
+		// public long maximumPoints(int[] a, int v) {
+		// 	int n = a.length; Set<Integer> s = new HashSet<>(); // marked idx
+		// 	long r = 0;
+		// }
+
+		// // 【亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹，就是一定要、一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+		// // 亲爱的表哥的活宝妹，知道昨天早上的这组题目，都是什么狗屁的、大数 BigInteger 等的【位操作】，
+		// // 可是亲爱的表哥的活宝妹，这几天身体困顿、休息受限，思路基本都对，感觉就是想不透彻。。
+		// public long countSubarrays(int[] a, int k) {
+		// 	int n = a.length, mk = 0, r [] = new int [n], cur = 0;
+		// 	for (int i = 0; i < 31; i++)
+		// 		if ((k & (1 << i)) > 0) mk |= (1 << i);
+		// 	for (int i = 0; i < n; i++) {
+		// 		cur = 0;
+		// 		for (int j = 0; j < 31; j++)
+		// 			if ((a[i] & (1 << j)) > 0)
+		// 				cur |= (1 << j);
+		// 		r[i] = cur;
+		// 	}
+		// 	// 数组的长短，不受限制，可以任意长度；只是要数个数
+		// 	// 先去解决：最有可能解决掉的、前一个问题
+		// }
+
+		
+// 		// 【亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹，就是一定要、一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 		public int numberOfAlternatingGroups(int[] a, int k) {
+// 			// BigInteger 要实现【位操作】：参考细节：
+// // //      2^63-1= 9223372036854775807;
+// // 			BigInteger tempVal = new BigInteger("3"); // 2049
+// // 			BigInteger divide = new BigInteger("2");
+// // 			BigInteger compareNum=new BigInteger("1");
+// // 			ArrayList<Byte> ResultArray = new ArrayList<Byte>();
+// // 			// 位遍历 算出第几位不为1的编码
+// // 			for (Byte i = 0;; i++) {
+// // 				// 判断是否除尽
+// // 				// BigInteger的equals方法参数类型为Object，
+// // 				// 但是这个类型必须是BigInteger类型才可以比较
+// // 				if (!tempVal.equals(compareNum)) {
+// // 					// 取整数和余数 
+// // 					BigInteger[] bigIntegers = tempVal.divideAndRemainder(divide);
+// // 					System.out.println(Arrays.toString(bigIntegers)); // 
+// // 					if (bigIntegers[1].equals(compareNum)) {
+// // 						ResultArray.add(i);
+// // 					}
+// // 					tempVal=bigIntegers[0];
+// // 				} else {
+// // 					ResultArray.add(i);
+// // 					break;
+// // 				}
+// // 			}
+// // 			System.out.println("ResultArray.size(): " + ResultArray.size());
+// // 			System.out.println(Arrays.toString(ResultArray.toArray()));
+// 			int n = a.length, j = 0, r = 0; // j: 右端点
+// 			// 【滑动窗口】：O(NK), 得用上次刚学到的，那什什么狗屁大数 BigInteger 标记位
+// 			List<Integer> cur = new ArrayList<>();
+// 			// BigInteger cur = (a[0] == 1 ? BigInteger.ONE : BigInteger.ZERO);
+// 			cur.add(a[0]);
+// 			for (j = 1; j < k; j++) {
+// 				// cur.shiftLeft(1);
+// 				// if (a[j] == 1) cur.add(BigInteger.ONE);
+// 				cur.add(a[j]);
+// 			}
+// 			// BigInteger tmp = BigInteger.valueOf(0);
+// 			// BigInteger mask = BigInteger.ONE.shiftLeft(k).subtract(BigInteger.ONE); // 制造一个：有V 个数位、每个数位都是 1 的大数 mask
+// 			// tmp = cur;
+// 			// if (tmp.shiftLeft(1).and(cur) == BigInteger.ZERO) r++;
+// 			if (custLiiAnd(cur)) r++;
+// 			for (int i = 1; i+k <= n+1; i++, j++) {
+// 				// System.out.println("\n i: " + i);
+// 				// System.out.println("cur: " + cur);
+// 				// cur.shiftLeft(1);
+// 				// cur = cur.and(mask);
+// 				cur.remove(0);
+// 				if (i == n-(k-1)) j = 0; // 仅只发生一次： i==n-(k-1) && j==n
+// 				// cur.add(a[j] == 1 ? BigInteger.ONE : BigInteger.ZERO);
+// 				cur.add(a[j]);
+// 				// System.out.println("cur: " + cur);
+// 				// tmp = cur;
+// 				// if (tmp.shiftLeft(1).and(cur) == BigInteger.ZERO) r++;
+// 				if (custLiiAnd(cur)) r++;
+// 			}
+// 			return r;
+// 		}
+// 		boolean custLiiAnd(List<Integer> l) { // 感觉，这个方法也写得有问题。。。今天上午不想再写了。。。
+// 			System.out.println("l.size(): " + l.size());
+// 			System.out.println(Arrays.toString(l.toArray()));
+// 			List<Integer> t = new ArrayList<>(l);
+// 			t.remove(0);
+// 			t.add(0);
+// 			int r = 0;
+// 			for (int i = 0; i < l.size()-1; i++)
+// 				if ((l.get(i) ^ t.get(i)) == 1) r++;
+// 			return r == l.size() + l.get(l.size()-1);
+// 		}
+
+		// // 【亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹，就是一定要、一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+		// public String getEncryptedString(String S, int k) {
+		// 	int n = S.length(), j = k % n;
+		// 	char [] s = S.toCharArray(), t = new char [n];
+		// 	for (int i = 0; i < n; i++, j++) {
+		// 		if (j == n) j = 0;
+		// 		t[i] = s[j];
+		// 	 }
+		// 	return new String(t);
+		// }
+
+		// // 【亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹，就是一定要、一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+		// public List<String> validStrings(int n) {
+		// 	l = new ArrayList<>(); this.n = n; 
+		// 	backtracing(0, -1, new StringBuilder(""));
+		// 	return l;
+		// }
+		// List<String> l; int n;
+		// void backtracing(int idx, int p, StringBuilder s) {
+		// 	if (idx == n) {
+		// 		if (!l.contains(new String(s)))
+		// 			l.add(new String(s));
+		// 		return ;
+		// 	}
+
+		// 	for (int j = 0; j < 2; j++) {
+		// 		if (j != p || j == p && j == 1) {
+		// 			s.append(j);
+		// 			backtracing(idx+1, j, s);
+		// 			s.deleteCharAt(s.length()-1);
+		// 		}
+		// 	}
+		// }		
+
+		// // 【亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹，就是一定要、一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+		// public int numberOfSubmatrices(char[][] a) {
+		// 	int m = a.length, n = a[0].length, r = 0;
+		// 	int [][] f = new int [m+1][n+1]; // preSum of matrix
+		// 	boolean [][] g = new boolean [m+1][n+1]; // boolean flag appearance of X
+		// 	for (int i = 1; i <= m; i++) 
+		// 		for (int j = 1; j <= n; j++) {
+		// 			f[i][j] = f[i-1][j] + f[i][j-1] - f[i-1][j-1] + (a[i-1][j-1] == 'X' ? 1 : (a[i-1][j-1] == 'Y' ? -1 : 0));
+		// 			if (a[i-1][j-1] == 'X' || g[i-1][j] || g[i][j-1])
+		// 				g[i][j] = true;
+		// 			if (g[i][j] && f[i][j] == 0) r++;
+		// 		}
+		// 	return r;
+		// }
+
 		// 【亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹，就是一定要、一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
-		public int minimumDiameterAfterMerge(int[][] ea, int[][] eb) {
-			n = ea.length+1; m = eb.length+1;
-			f = new ArrayList[n]; g = new ArrayList[m];
-			Arrays.setAll(f, z -> new ArrayList<>());
-			Arrays.setAll(g, z -> new ArrayList<>());
-			if (n > 1) buildTree(ea, f);
-			if (m > 1) buildTree(eb, g);
-			// assume 根节点在 0 节点：求最大、次大深度
-			l = new int [2]; r = new int [2];
-			Arrays.fill(l, -1); Arrays.fill(r, -1);
-			maxDepth(f, l, 0, 0, -1); maxDepth(g, r, 0, 0, -1);
-			System.out.println(Arrays.toString(l));
-			System.out.println(Arrays.toString(r));
-			if (l[1] == -1) l[0] = l[0] / 2 + l[0] % 2;
-			if (r[1] == -1) r[0] = r[0] / 2 + r[0] % 2;
-			return l[0] + 1 + r[0]; // 这里还有点儿机关，没想透彻。。。
+		// 感觉是，狗屁的【记忆化深搜＋字典树】？遍历【目标字符串】＋遍历【dfs 深搜字典树】，【记忆化深搜】找最小代价。。。702/803 passed ...
+		// TLE 的情况下：亲爱的表哥的活宝妹，就得去想，字典树，可能加反了。。。加S 的所有子串，或至少另一棵字典树，加S 的所有子串，用来提速。。。
+		// 上面不对：Deque<Integer> [26] q  = new ArrayDeque[26]; 用来记S 里【ab...z】的所有下标
+		public class Trie {
+			public  class Node {
+				char c;
+				boolean f;
+				int i; // 用来记：串的长度
+				Node [] n;
+				public Node(char c, boolean f) {
+					this.c = c; this.f = f;
+					n = new Node[26];
+					i = -1;
+				}
+			}
+			Node r;
+			public Trie() {
+				r = new Node(' ', false);
+			}
+			public void insert(String S) {
+				int n = S.length();
+				char [] s = S.toCharArray();
+				Node f = r;
+				for (int i = 0; i < n; i++) {
+					int j = s[i] - 'a';
+					if (f.n[j] == null)
+						f.n[j] = new Node(s[i], false);
+					f = f.n[j];
+				}
+				f.f = true;
+				// f.i = (f.i == -1 || f.i > k ? k : f.i);
+				f.i = S.length();
+			}
+			List<Integer> searchAllLength(int i) { // 遍历【字典树】：找 s[] 从下标 i 开始，所有匹配的字符串的下标
+				List<Integer> l = new ArrayList<>();
+				Node f = r;
+				int j = i;
+				while (j < s.length && f.n[s[j]-'a'] != null) {
+					if (f.n[s[j]-'a'].f) l.add(f.n[s[j]-'a'].i);
+					f = f.n[s[j]-'a'];
+					j++;
+				}
+				return l;
+			}
 		}
-		List<Integer> [] f, g;
-		int m, n;
-		int [] l, r;
-		void maxDepth(List<Integer> [] f, int [] l, int d, int u, int p) {
-			int cnt = 0;
-			for (int v : f[u]) {
-				if (v == p) continue;
-				cnt++;
-				maxDepth(f, l, d+1, v, u);
+		public int minimumCost(String S, String[] sa, int[] a) {
+			n = S.length(); this.sa = sa; this.a = a; this.S = S; 
+			t = new Trie(); ss = new Trie();
+			s = S.toCharArray();
+			q = new ArrayDeque[26];
+			Arrays.setAll(q, z -> new ArrayDeque<Integer>());
+			for (int i = 0; i < n; i++)
+				q[s[i]-'a'].offerLast(i);
+			for (int i = 0; i < sa.length; i++) {
+				if (!isSubString(i)) continue;
+				if (ms.containsKey(sa[i])) {
+					ms.put(sa[i], Math.min(ms.get(sa[i]), a[i]));
+					continue;
+				}
+				if (sa[i].length() <= n)
+				t.insert(sa[i]);
+				ms.put(sa[i], a[i]);
 			}
-			if (cnt == 0) {
-				if (l[0] == -1) l[0] = d;
-				else if (d >= l[0]) {
-					if (d != l[0])
-						l[1] = l[0];
-					if (d > l[0])
-						l[0] = d;
-				} else if (d > l[1])
-					l[1] = d;
-			}
+			f = new Integer [n];
+			int r = dfs(0); 
+			return r >= Integer.MAX_VALUE / 2 ? -1 : r;
 		}
-		void buildTree(int [][] a, List<Integer> [] f) {
-			for (int [] e : a) {
-				int u = e[0], v = e[1];
-				f[u].add(v);
-				f[v].add(u);
+		boolean isSubString(int i) { // i:idx in sa
+			boolean valid = true;
+			Deque<Integer> sq = q[sa[i].charAt(0)-'a'];
+			for (int j : sq) {
+				valid = true;
+				for (int k = j; k < j+sa[i].length(); k++) 
+					if (s[k] != sa[i].charAt(k-j)) {
+						valid = false;
+						break;
+					}
+				if (valid)
+					return true;
 			}
+			return false ;
+		}
+		Trie ss, t;
+		int n; String [] sa; int [] a; char [] s; String S;
+		Deque<Integer> [] q = new ArrayDeque[26];
+		Map<String, Integer> ms = new HashMap<>(); // 如果：记另外字典里，就需要【字典树返回长度。。】
+		Integer [] f;
+		int dfs(int i) {
+			if (i == n) return 0;
+			List<Integer> l = t.searchAllLength(i);
+			if (l.size() == 0) return f[i] = Integer.MAX_VALUE / 2;
+			int r = Integer.MAX_VALUE / 2;
+			for (int j : l) 
+				r = Math.min(r, ms.get(S.substring(i, i+j)) + dfs(i+j));
+			return f[i] = r;
 		}
 }
 // 亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！
     public static void main (String[] args) {
 		Solution s = new Solution ();
 
-		int [][] a = new int [][] {{1,0},{2,3},{1,4},{2,1},{2,5}};
-		int [][] b = new int [][] {{4,5},{2,6},{3,2},{4,7},{3,4},{0,3},{1,0},{1,8}};
+		String a = "xmjpkowhuefntanqvygzukqlxrrpsbuscjesdxilsjodkjjzhfrzymiaeedjsfpquiu";
+		String [] b = new String []  {"u","m","i","d","busc","z","j","s","huefntanqvygzuk","d","qlxrrpsbuscjesdxilsjodkjj","nqvygzukqlxrrpsbuscjesdxilsjodkjjzhfrzymiaee","h","c","o","s","l","sjodkjjzhfrzymi","aeedjsfpqu","i","u","kowhue","pkowhuefntanqvygzukqlxrrpsbus","x","j","djsfpquiu","dxilsjodkjjzhfrzymiaeedjsfpqu","j","jodkjjzhfrzymia","xrrpsbuscjesdxilsj","r","f","iu","k","frzymiaeedjsf","zymiaee","djsf","j","mjpkowh","d","j","x","i","miaeed","fntanqvygzukqlxrrpsbuscjesdxilsjodkjjzhfrzymiaeedjsfpq","lxrrpsbuscjesdxilsjod","jsfpqu","e"};
+		int []  c = new int []  {41,2,23,44,1,2,31,12,4,43,17,16,19,34,36,36,16,17,30,19,34,5,43,28,43,31,1,21,26,2,23,10,48,24,7,44,1,46,26,25,24,31,23,13,7,13,24,41};
 
-		int r = s.minimumDiameterAfterMerge(a, b);
+		int r = s.minimumCost(a, b, c);
 		System.out.println("r: " + r);
     }
 }
@@ -320,6 +576,50 @@ public class cmp {
 // 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
 // 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
 // 【亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+// 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
 // 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
 // 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
 // 【爱表哥，爱生活！！！任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
