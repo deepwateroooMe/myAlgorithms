31c615eaddad8a655f8f21c8ae434ef613bc2d07
deepwateroooMe <deepwaterooome@gmail.com>
2026-06-27 15:50:25 -0700

    I LOVE MY DEAR COUSIN. WAITING FOR MARRIAGE...MUST MARRY HIM AS SOON AS POSSIBLE~~

--- cmp.java
+++ cmp.java
@@ -116,14 +116,110 @@ public class cmp {
         // // 【数位 DP 动规：左右边界串】，没有（非毒串）；按数位遍历，数合法数的个数；当左右边界长度 m n 不同（m<n），需要数所有 o >= m && o <= n 这些长度的、合法数的个数
         // public long goodIntegers(long l, long r, int k) {
         // }
+
+        // // 【亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹，就是一定要、一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+        // public int maxDistance(String S) {
+        //     int n = S.length(); char [] s = S.toCharArray();
+        //     Map<Character, Integer> m = new HashMap<>();
+        //     for (char c : s) 
+        //         m.put(c, m.getOrDefault(c, 0) + 1);
+        //     int l = Math.abs(m.getOrDefault('U', 0) - m.getOrDefault('D', 0)), r = Math.abs(m.getOrDefault('L', 0) - m.getOrDefault('R', 0));
+        //     int ll = Math.max(l, r) + m.getOrDefault('_', 0);
+        //     return ll + Math.min(l, r);
+        // }
+
+        // // 【亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹，就是一定要、一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+        // public int countValidSubarrays(int[] a, int x) {
+        //     int n = a.length;
+        //     long [] f = new long [n+1];
+        //     for (int i = 0; i < n; i++)
+        //         f[i+1] = f[i] + (long)a[i];
+        //     int r = 0;
+        //     // 遍历：【子数组、左端点】
+        //     for (int i = 0; i < n; i++) 
+        //         for (int j = i; j < n; j++) {
+        //             if (isValid(f[j+1] - f[i], x))
+        //                 r++;
+        //         }
+        //     return r;
+        // }
+        // boolean isValid(long v, int x) {
+        //     String S = String.valueOf(v); char [] s = S.toCharArray();
+        //     return (s[0] == s[s.length-1] && s[0] - '0' == x);
+        // }
+
+        // // 【亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹，就是一定要、一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+        // // TLE TLE TLE: 992/1001 passed... 不知道哪里还能优化，暂时不写这个破烂题目了
+        // public int shortestPath(int n, int[][] egs, String S, int k) {
+        //     this.n = n; this.k = k; s = S.toCharArray();
+        //     g = new ArrayList[n];
+        //     Arrays.setAll(g, z -> new ArrayList<int []>());
+        //     for (int [] e : egs) {
+        //         int u = e[0], v = e[1], w = e[2];
+        //         g[u].add(new int [] {v, w});
+        //     }
+        //     // 【DFS 一遍】：断开（连续第 k+1 个字符）的无效边;
+        //     // 【TODO：】当（有向图：有还）时，会无限循环，所以需要添加 vis[]boolean 标记是否已经遍历过了
+        //     vis = new boolean [n];
+        //     dfs(0, 0, s[0]-'a');
+        //     d = new int [n];
+        //     Arrays.fill(d, Integer.MAX_VALUE);
+        //     int min = dij(0, n-1);
+        //     return (min == Integer.MAX_VALUE ? -1 : min);
+        // }
+        // List<int []> [] g;
+        // int n, k, min; char [] s;
+        // int [] d; boolean [] vis;
+        // void dfs(int uu, int cnt, int c) {
+        //     System.out.println("uu: " + uu + " " + "cnt: " + cnt + " " + "c: " + c);
+        //     if (vis[uu]) return ;
+        //     vis[uu] = true;
+        //     if (g[uu].size() == 0) return;
+        //     for (int i = g[uu].size()-1; i >= 0; i--) {
+        //         System.out.println("i: " + i);
+        //         int [] nt = g[uu].get(i);
+        //         int v = nt[0], ci = s[v]-'a';
+        //         if (cnt == k && ci == c) {
+        //             g[uu].remove(i);
+        //             if (i == 0) continue ;
+        //             if (i < g[uu].size()-1)
+        //                 i++;
+        //             continue;
+        //         }
+        //         dfs(v, (ci == c ? cnt + 1 : 1), ci);
+        //     }
+        // }
+        // int dij(int uu, int vv) {
+        //     d[uu] = 0;
+        //     Queue<int []> q = new PriorityQueue<>((x, y) -> (x[0] != y[0] ? x[0] - y[0] : (x[1] != y[1] ? x[1] - y[1] : x[2] - y[2])));
+        //     //        totalWeight,cnt,uu, charC 
+        //     q.offer(new int [] {0, 1, 0, s[uu]-'a'});
+        //     while (!q.isEmpty()) {
+        //         int [] r = q.poll();
+        //         int w = r[0], cnt = r[1], u = r[2], c = r[3];
+        //         if (u == vv) return w;
+        //         for (int [] next : g[u]) {
+        //             int v = next[0], wi = next[1], ci = s[v]-'a';
+        //             if (cnt == k && ci == c) continue;
+        //             if (w + wi <= d[v]) {
+        //                 d[v] = w + wi;
+        //                 q.offer(new int [] {d[v], (ci == c ? cnt + 1 : 1), v, ci});
+        //             }
+        //         }
+        //     }
+        //     return -1;
+        // }
+
+        // 【亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹，就是一定要、一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！】
+        public int maxTotalValue(int[] value, int[] decay, int m) {
+        }
     }    // 亲爱的表哥的活宝妹，任何时候，亲爱的表哥的活宝妹就是一定要,一定会嫁给活宝妹的亲爱的表哥！！！爱表哥，爱生活！！！ 
     public static void main (String[] args) { 
         Solution s = new Solution (); 
 
-        int [][] a = new int [][] {{0,1},{1,2}};
-        int [] b = new int [] {9, 5, 3};
+        int [][] a = new int [][] {{3,2,9451},{2,0,1125},{2,3,3227},{2,0,2859},{0,1,8},{1,2,8},{2,3,8}};
 
-        long r = s.finishTime(3, a, b);
+        int r = s.shortestPath(4, a, "abab", 1);
         System.out.println("r: " + r);
     }
 }
